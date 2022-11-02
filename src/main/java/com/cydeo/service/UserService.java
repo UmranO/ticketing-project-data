0f69ca622db37e9e78d1c12ef9f2ada2d6c19db4
Umran Ozman <umonal@yahoo.com>
2022-11-02 15:28:04 -0400

    UserService interface is updated

--- src/main/java/com/cydeo/service/UserService.java
+++ src/main/java/com/cydeo/service/UserService.java
@@ -1,4 +1,15 @@
 package com.cydeo.service;
 
-public interface UserService {
+
+import com.cydeo.dto.UserDTO;
+
+import java.util.List;
+
+public interface UserService  {
+
+    List<UserDTO> listAllUsers();
+    UserDTO findByUserName(String username);
+    void save(UserDTO user);
+    void deleteByUserName(String username);
+
 }
