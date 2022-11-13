2097334faf5857ce88e98cf66c5fbe72d79bc86a
Umran Ozman <umonal@yahoo.com>
2022-11-13 14:32:12 -0500

    UserServiceImpl updated

--- src/main/java/com/cydeo/service/impl/UserServiceImpl.java
+++ src/main/java/com/cydeo/service/impl/UserServiceImpl.java
@@ -48,4 +48,20 @@ public class UserServiceImpl implements UserService {
     public void deleteByUserName(String username) {
 
     }
+
+    @Override
+    public UserDTO update(UserDTO user) {
+
+        //Find current user
+        User user1 = userRepository.findByUserName(user.getUserName());  //has id
+        //Map update user dto to entity object
+        User convertedUser = userMapper.convertToEntity(user);   // has id?
+        //set id to the converted object
+        convertedUser.setId(user1.getId());
+        //save the updated user in the db
+        userRepository.save(convertedUser);
+
+        return findByUserName(user.getUserName());
+
+    }
 }
