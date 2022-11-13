23e47304e1c46e2ab167d31d6f54761b4a26c4c3
Umran Ozman <umonal@yahoo.com>
2022-11-13 11:24:36 -0500

    UserController updated

--- src/main/java/com/cydeo/controller/UserController.java
+++ src/main/java/com/cydeo/controller/UserController.java
@@ -6,10 +6,7 @@ import com.cydeo.service.UserService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
 @Controller
 @RequestMapping("/user")
@@ -51,17 +48,17 @@ public class UserController {
         return "redirect:/user/create";
 
     }
-//
-//    @GetMapping("/update/{username}")
-//    public String editUser(@PathVariable("username") String username, Model model) {
-//
-//        model.addAttribute("user", userService.findById(username));
-//        model.addAttribute("roles", roleService.findAll());
-//        model.addAttribute("users", userService.findAll());
-//
-//        return "/user/update";
-//
-//    }
+
+    @GetMapping("/update/{username}")
+    public String editUser(@PathVariable("username") String username, Model model) {
+
+        model.addAttribute("user", userService.findByUserName(username));
+        model.addAttribute("roles", roleService.listAllRoles());
+        model.addAttribute("users", userService.listAllUsers());
+
+        return "/user/update";
+
+    }
 //
 //    @PostMapping("/update")
 //    public String updateUser(@Valid @ModelAttribute("user") UserDTO user, BindingResult bindingResult, Model model) {
