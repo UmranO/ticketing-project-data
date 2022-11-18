66fafbbde6f774e0ab78aa5ceb95a3e41fd56fee
Umran Ozman <umonal@yahoo.com>
2022-11-18 08:17:19 -0500

    UserServiceImpl is updated

--- src/main/java/com/cydeo/service/impl/UserServiceImpl.java
+++ src/main/java/com/cydeo/service/impl/UserServiceImpl.java
@@ -74,4 +74,13 @@ public class UserServiceImpl implements UserService {
         userRepository.save(user);
 
     }
+
+    @Override
+    public List<UserDTO> listAllByRole(String role) {
+        List<User> users = userRepository.findByRoleDescriptionIgnoreCase(role);
+
+        return users.stream().map(userMapper::convertToDto).collect(Collectors.toList());
+
+
+    }
 }
