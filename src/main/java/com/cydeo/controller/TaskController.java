960f3fd293c9bbf3fa7825b38d6c3e2680a242ed
Umran Ozman <umonal@yahoo.com>
2022-11-26 18:15:03 -0500

    TaskController updated / Commented unnecessary method deleted

--- src/main/java/com/cydeo/controller/TaskController.java
+++ src/main/java/com/cydeo/controller/TaskController.java
@@ -136,16 +136,6 @@ public String updateTask(@ModelAttribute("task") TaskDTO task, BindingResult bin
         return "redirect:/task/employee/pending-tasks";
 
 
-////    @PostMapping("/update/{taskId}")
-////    public String updateTask(@PathVariable("taskId") Long taskId, TaskDTO task) {
-////
-////        task.setId(taskId);
-////        taskService.update(task);
-////
-////        return "redirect:/task/create";
-////
-////    }
-//
     }
 }
 
