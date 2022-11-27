d0b03adbf22f4aadf5668b40ec9d09b3dd68fb2d
Umran Ozman <umonal@yahoo.com>
2022-11-26 20:07:00 -0500

    TasServiceImpl updated /listAllNonCompletedByAssignedEmployee method impl. given

--- src/main/java/com/cydeo/service/impl/TaskServiceImpl.java
+++ src/main/java/com/cydeo/service/impl/TaskServiceImpl.java
@@ -134,5 +134,11 @@ public class TaskServiceImpl implements TaskService {
         return tasks.stream().map(taskMapper::convertToDto).collect(Collectors.toList());
     }
 
+    @Override
+    public List<TaskDTO> listAllNonCompletedByAssignedEmployee(UserDTO assignedEmployee) {
+        List<Task> tasks = taskRepository
+                .findAllByTaskStatusIsNotAndAssignedEmployee(Status.COMPLETE, userMapper.convertToEntity(assignedEmployee));
+        return tasks.stream().map(taskMapper::convertToDto).collect(Collectors.toList());
+    }
 
 }
